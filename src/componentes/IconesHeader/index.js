38566a703bc05aebb17babeee332e6f1cc1797c3
Rodrigo P. Oliveira <ro.oliveira1605@gmail.com>
2026-01-05 19:22:16 -0300

    .

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,17 +1,26 @@
-import './estilo.css'
 import perfil from '../../imagens/perfil.svg'
 import sacola from '../../imagens/sacola.svg'
+import styled from 'styled-components'
+
+const Icone = styled.li`
+     margin-right: 40px;
+     width: 25px;
+`
+const Icones = styled.ul`
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+`
 
 const icones = [perfil, sacola]
 
 function IconesHeader() {
     return (
-        <ul className='icones'>
+        <Icones>
             {icones.map((icone) => (
-                <li className='icone'><img src={icone} alt='img'></img></li>
+                <Icone><img src={icone} alt='img'></img></Icone>
             ))}
-
-        </ul>
+        </Icones>
     )
 }
 
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,15 +1,23 @@
-import './estilo.css';
 import logo from '../../imagens/logo.svg';
+import styled from 'styled-components'
+
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px
+`
+const LogoImg = styled.img`
+     margin-right: 10px;
+`
 
 function Logo() {
     return (
-        <div className="logo">
-            <img
+        <LogoContainer>
+            <LogoImg
                 src={logo}
                 alt="logo" className='logo-img'
-            ></img>
+            ></LogoImg>
             <p><strong>Alura</strong>Books</p>
-        </div>
+        </LogoContainer>
     )
 }
 
