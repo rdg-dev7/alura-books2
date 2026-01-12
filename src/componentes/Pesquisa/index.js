97a7bfdbcc98e1093c833aa242c4639838583c5d
Rodrigo P. Oliveira <ro.oliveira1605@gmail.com>
2026-01-12 19:34:47 -0300

    .

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,6 +1,7 @@
 import Input from '../Input'
 import styled from 'styled-components'
 import { useState } from 'react'
+import { livros } from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -22,7 +23,7 @@ const Subtitulo = styled.h3`
     margin-bottom: 40px;
 `
 function Pesquisa() {
-    const [textoDigitado, setTextoDigitado] = useState('')
+    const [livrosPesquisados, setLivrosPesquisados] = useState([])
 
     return (
         <PesquisaContainer>
@@ -30,9 +31,18 @@ function Pesquisa() {
             <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
             <Input
                 placeholder="Escreva sua próxima leitura"
-                onBlur={evento => setTextoDigitado(evento.target.value)}
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter(livro => livro.nome.includes(textoDigitado))
+                    setLivrosPesquisados(resultadoPesquisa)
+                }}
             />
-            <p>{textoDigitado}</p>
+            {livrosPesquisados.map(livro => (
+                <div>
+                    <p>{livro.nome}</p>
+                    <img src={livro.src} />
+                </div>
+            ))}
         </PesquisaContainer>
 
     )
